24ff238a3046675871e43cc932352421425d00cb
Your Name <you@example.com>
2019-01-25 21:03:35 -0500

    Correcion: mostrar valor a pagar

--- PublicParkingGit/src/vistaControl/ParqueoPublicoController.java
+++ PublicParkingGit/src/vistaControl/ParqueoPublicoController.java
@@ -64,6 +64,6 @@ public class ParqueoPublicoController {
     	horaFinalTextField.setText(horaFinal);
     	double valorAPagar = parqueoPublico.calcularValorAPagar();
     	// To modify line
-    	valorAPagarTextField.setText(CURRENCY.format(valorAPagar+""));
+    	valorAPagarTextField.setText(CURRENCY.format(valorAPagar));
     }    
 }
